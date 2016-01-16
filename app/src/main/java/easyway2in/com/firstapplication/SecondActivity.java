9f52767abd5ae5a1c7e3a690e98cc8066012a56e
tawazlwb <tawazlwb@gmail.com>
2016-01-16 14:57:22 +0100

    Initial commit

---
+++ app/src/main/java/easyway2in/com/firstapplication/SecondActivity.java
@@ -0,0 +1,15 @@
+package easyway2in.com.firstapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by MMD on 16/01/2016.
+ */
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
